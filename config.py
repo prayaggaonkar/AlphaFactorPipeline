a4cea7af03d7a5535282e7614723b5ce0a625bbe
prayaggaonkarr <gaonkarprayag@gmail.com>
2026-07-20 18:34:00 -0700

    fixing training window

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 ## Change these values to experiment with different setups
 
 UNIVERSE_SIZE = 50        # number of stocks (start small, expand later)
-START_DATE    = "2024-01-01"
-END_DATE      = "2025-01-01"
+START_DATE    = "2018-01-01"
+END_DATE      = "2024-01-01"
 FORWARD_DAYS  = 5         # predict 5-day future returns
 REBAL_FREQ    = 5         # rebalance every 5 trading days
 TRAIN_WINDOW  = 252       # 1 year of training data
